90441104dc1e687980b8e6ccdb85dc789073b12c
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-08-12 13:18:55 -0400

    Objective3Lab2 Completed.

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+
+public class Objective3Lab2{
+	public static void main (String[] args) {
+	double side1 = 10.0;
+	double side2 = 8.0;
+    double hypothenuse = Math.sqrt(side1 * side1 + side2 + side2); 
+    System.out.println("The hypothenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypothenuse);
+   }
+}
