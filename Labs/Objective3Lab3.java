be4e8347484b6f5412f7ac4c75790d195f0b43ca
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-08-12 14:52:15 -0400

    Objective3Lab3 Completed

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,9 @@
+public class Objective3Lab3{
+	public static void main(String[] args){
+		int first = (int)(Math.random() * 6);
+		int second = (int)(Math.random() * 6);
+		int third =  (int)(Math.random() * 6);
+		int sum = first + second + third; 
+		System.out.println(first + "+" + second + "+" + third + "=" +sum);
+	}
+}
