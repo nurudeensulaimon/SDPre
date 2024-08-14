3c22e746c9d50e262f44e38e8c64f963526a0a99
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-08-14 16:08:39 -0400

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+	public static void main(String[] args){
+		int sum_total = 0;
+		int counter = 1;
+
+		while (counter <= 20){
+			sum_total += counter;
+            counter++;
+		}
+		System.out.println(sum_total);
+	}
+}
